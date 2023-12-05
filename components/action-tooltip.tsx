f47304b50d7eb1a38f6ba4b6b03721ad705bbbd8
Shivam Narkar <shivamnarkar47@gmail.com>
2023-12-05 21:34:50 +0530

    Create Sidebar and Functionality

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface ActionTooltipProps {
+  label: String;
+  children: React.ReactNode;
+  side?: "left" | "right" | "bottom" | "top";
+  align?: "start" | "center" | "end";
+}
+
+const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ActionTooltip;
